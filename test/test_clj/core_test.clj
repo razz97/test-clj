631b77febe28983e6f67f13ea217b3017fac0082
razz <abou@oneboxtds.com>
2020-07-29 03:18:15 +0200

    tests

--- test/test_clj/core_test.clj
+++ test/test_clj/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [test-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sma-test
+  (testing "Simple moving average test"
+    (is (= (sma 5 [5 1 4 3 9 21 33 9 2 10]) [22/5 38/5 14 15 74/5 15]))))
+
+(deftest ema-test
+  (testing "Exponential moving average test"
+    (is (= (ema 5 [5 1 4 3 9 21]) [5 11/3 34/9 95/27 433/81 2567/243]))))
+
+(ema-test)
